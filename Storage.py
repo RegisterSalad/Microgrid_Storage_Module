69480720313dbf5fbbccd918921693d176846499
abishopgylys <abishopgylys@gmail.com>
2022-06-08 15:30:50 -0400

    Documented attributes

--- Storage.py
+++ Storage.py
@@ -37,7 +37,7 @@ class StorageSuite:
             for row in reader:
                 self.device_data[row['type']] = row
         for device in self.device_data:
-            self.storage_suite[device] = Storage(type=device)
+            self.storage_suite[device] = Storage(ss=self, type=device)
 
 #string.replace(old, new) (immutable) [replace() syntax]
 
@@ -52,17 +52,17 @@ class Storage:
         self.CAP = cap # in kWh
         self.POWER = power # in kW
         if self.POWER == None:
-            self.POWER = cap * ss.device_data['max_cont_discharge']
-        self.start_window = start_window
-        self.end_window = end_window
+            self.POWER = cap * ss.device_data['max_cont_discharge'] # uses default power to capacity ratio for given type
+        self.START_WINDOW = start_window # start time that device can be used (V2G), in seconds of the day out of 86,400
+        self.END_WINDOW = end_window # end time that device can be used (V2G), in seconds of the day out of 86,400
         if bool(ss.device_data['cap_power_ind']):
-            self.capital_cost = capital_cost()
+            self.CAPITAL_COST = capital_cost() # if capital cost is a function of both power and capacity, in USD
         else:
-            self.capital_cost = ss.device_data['capital_cost'] * cap
-        self.MAX_SOC = ss.device_data['max_charge']
-        self.MIN_SOC = ss.device_data['min_charge']
-        self.MAX_SOC_CAP = ss.device_data['max_charge'] * cap
-        self.MIN_SOC_CAP = ss.device_data['min_charge'] * cap
+            self.CAPITAL_COST = ss.device_data['capital_cost'] * cap # if capital cost is primarily a function of capacity only, in USD
+        self.MAX_SOC = ss.device_data['max_charge'] # maximum charge as a proportion of capacity
+        self.MIN_SOC = ss.device_data['min_charge'] # minimum charge as a proportion of capacity
+        self.MAX_SOC_CAP = ss.device_data['max_charge'] * cap # maximum charge in kWh
+        self.MIN_SOC_CAP = ss.device_data['min_charge'] * cap # minimum charge in kWh
         #self.MAX_CHARGE_RATE = ss.device_data['max_charge_rate'] * 
         #self.MIN_CHARGE_RATE = ss.device_data['min_charge_rate']
         #self.MAX_CONT_DISCHARGE = ss.device_data['max_cont_discharge'] 
@@ -71,30 +71,32 @@ class Storage:
 
      
         #calculated
-        self.PEAK_DISCHARGE = ss.device_data['max_peak_discharge'] * self.POWER # assumed 10s capability
+        self.PEAK_DISCHARGE = ss.device_data['max_peak_discharge'] * self.POWER # assumed 10s peak capability, in kW
         self.FORMULA_EFF_CHARGE = ss.device_data['eff_charge'].replace("x", "self.soc")
-        def eff_charge():
+        def eff_charge(): # charge effiency function
             return eval_expr(self.FORMULA_EFF_CHARGE.replace("self.soc", str(self.soc)))
         # may need to adjust formulae in current CSV to take proportional SOC rather than current_charge
         # would just have to divide each x in the formula by the capacity of the flywheel used for modelling, probably 29kWh
         self.FORMULA_EFF_DISCHARGE = ss.device_data['eff_discharge'].replace("x", "self.soc")
-        def eff_discharge():
+        def eff_discharge(): # discharge effiency function
             return eval_expr(self.FORMULA_EFF_DISCHARGE.replace("self.soc", str(self.soc)))
         
         self.FORMULA_SELF_DISCHARGE = ss.device_data['self_discharge'].replace("x", "self.soc")
-        def self_discharge():
+        def self_discharge(): # self-discharge rate, in kW
             return eval_expr(self.FORMULA_SELF_DISCHARGE.replace("self.soc", str(self.soc)))
 
         self.FORMULA_CAPITAL_COST = ss.device_data['capital_cost']
-        def capital_cost():
+        def capital_cost(): # independent capacity and power capital cost formula
             return eval_expr(self.FORMULA_CAPITAL_COST.replace("X", str(self.CAP)).replace("Y", str(self.POWER))) * self.CAP
 
             
-        self.marginal_cost = ss.device_data['marginal_cost']
-        self.ramp_speed = ss.device_data['ramp_speed']
-        self.resp_time = ss.device_data['resp_time']
-        self.soc = 0
-        self.current_charge = self.soc * self.CAP
+        self.MARGINAL_COST = ss.device_data['marginal_cost'] # cost to use device per kW in/out, in USD
+        #self.ramp_speed = ss.device_data['ramp_speed']
+        self.resp_time = ss.device_data['resp_time'] # time it takes for device to realize command, in seconds
+        self.soc = 0 # state of charge as a proportion of capacity
+        self.soc_cap = 0 # state of charge in kWh
+        def current_charge():
+            return self.soc * self.CAP
 
         #user/AI-defined
         pass
@@ -111,5 +113,7 @@ if __name__ == "__main__":
 #or even separate suites for each?
 
 # NEXT STEPS:
-# 1. Decide whether to convert each value to flat numbers or keep as proportions
-# 2. Normalize formulae that were based on a specific unit (i.e. cap of 29kWh)
+# 1. Normalize formulae that were based on a specific unit (i.e. cap of 29kWh)
+# 2. Research aggregator response time
+# 3. Test formulae
+# 4. Test initialization of StorageSuite
